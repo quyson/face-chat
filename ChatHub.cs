4ca3940145ec6b9efc9cfd270dd05ccb3ee906b7
unknown <quyson.nguyen.1999@gmail.com>
2023-07-08 22:31:48 -0700

    Create Chathub SignalR

---
+++ ChatHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace facechat
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+        public override async Task OnConnectedAsync()
+        {
+            string connectionId = Context.ConnectionId;
+            string username = Context.User.Identity.Name;
+
+            await Clients.Others.SendAsync("JoinUser", username);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            string connectionId = Context.ConnectionId;
+            string username = Context.User.Identity.Name;
+
+            await Clients.Others.SendAsync("LeaveUser", username);
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
