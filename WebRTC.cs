327fcfe9815b9b1cb9a0093a8b3ee98db8f03a06
unknown <quyson.nguyen.1999@gmail.com>
2023-07-22 17:26:51 -0700

    Fix recipient bug

--- WebRTC.cs
+++ WebRTC.cs
@@ -1,18 +1,54 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace facechat
 {
     public class WebRTC : Hub
     {
-        public async Task Offer(string connectionId, string sdpOffer)
+        private static readonly ConcurrentDictionary<string, string> ActiveConnections = new ConcurrentDictionary<string, string>();
+
+        public override Task OnConnectedAsync()
         {
-            await Clients.Client(connectionId).SendAsync("ReceiveOffer", Context.ConnectionId, sdpOffer);
+            ActiveConnections.TryAdd(Context.ConnectionId, "lol");
+            Console.WriteLine($"New Connection {Context.ConnectionId}");
+            return base.OnConnectedAsync();
         }
 
-        public async Task Answer(string connectionId, string sdpOffer)
+        public override Task OnDisconnectedAsync(Exception exception)
         {
-            await Clients.Client(connectionId).SendAsync("ReceiveAnswer", Context.ConnectionId, sdpOffer);
+            ActiveConnections.TryRemove(Context.ConnectionId, out _);
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        private bool IsValidConnection(string connectionId)
+        {
+            return ActiveConnections.ContainsKey(connectionId);
+        }
+
+        public async Task Offer(string connectionId, string sdpOffer)
+        {
+            Console.WriteLine(sdpOffer);
+            Console.WriteLine(connectionId);
+
+            if (IsValidConnection(connectionId))
+            {
+                await Clients.Client(connectionId).SendAsync("ReceiveOffer", Context.ConnectionId, sdpOffer);
+                Console.WriteLine("sent");
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+
+                
+            
+        }
+
+        public async Task Answer(string connectionId, string sdpAnswer)
+        {
+            await Clients.Client(connectionId).SendAsync("ReceiveAnswer", Context.ConnectionId, sdpAnswer);
         }
 
         public async Task IceCandidate(string connectionId, string candidate)
